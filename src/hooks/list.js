31b41ffbeab919c2ae0ce9d52a37515c385d5f24
Partha Das <parthodasm@gmail.com>
2026-01-30 23:42:32 +0600

    Comment out cloudinary configuration and related functions

--- src/hooks/list.js
+++ src/hooks/list.js
@@ -1,25 +1,25 @@
-import cloudinary from "cloudinary";
+// import cloudinary from "cloudinary";
 
-cloudinary.v2.config({
-  cloud_name: "dkmwu9ec1",
-  api_key: "887557929275997",
-  api_secret: "siwECNfbaiBEP4JqNa9EZpbyBIs",
-});
+// cloudinary.v2.config({
+//   cloud_name: "dkmwu9ec1",
+//   api_key: "887557929275997",
+//   api_secret: "siwECNfbaiBEP4JqNa9EZpbyBIs",
+// });
 
-async function findEverything() {
-  try {
-    const result = await cloudinary.v2.search
-      .expression("folder:Partner/*") // This looks for anything inside 'organizer'
-      .execute();
+// async function findEverything() {
+//   try {
+//     const result = await cloudinary.v2.search
+//       .expression("folder:Partner/*") // This looks for anything inside 'organizer'
+//       .execute();
 
-    console.log("Total files found:", result.total_count);
-    console.log(
-      "URLs:",
-      result.resources.map((r) => r.secure_url),
-    );
-  } catch (error) {
-    console.error("Search Error:", error);
-  }
-}
+//     console.log("Total files found:", result.total_count);
+//     console.log(
+//       "URLs:",
+//       result.resources.map((r) => r.secure_url),
+//     );
+//   } catch (error) {
+//     console.error("Search Error:", error);
+//   }
+// }
 
-findEverything();
+// findEverything();
